4e31e50bdad53e83e82e6ad8b35a017268b10e9e
kharel001 <kharelmanish18@gmail.com>
2021-07-25 14:59:36 +0545

    Input fields added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import Login from './Login'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return (
+    <Login />
+  )
 }
 
 export default MyApp
